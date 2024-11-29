9bdf2e15f0c47e909b1111775dab5cca95ab73ad
Samarth Shendre <156545091+i-m-samarth-cs@users.noreply.github.com>
2024-11-29 22:40:22 +0530

    Add files via upload

---
+++ Element Acces in Multi.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main() {
+  int matrix[4][4] = {{14, 10, 6, 4}, {3, 7, 18, 11}, {13, 9, 5, 17}, {19, 12, 2, 1}}; 
+  int sum = 0;
+
+  // Checkpoint 1 code goes here.
+  printf("%i",matrix[4][2]);
+  // Checkpoint 2 code goes here.
+}
